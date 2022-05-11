716be73a91e4cc52da02c3c81074c2c0d3caf79e
kimsy030 <kimsy6675@gmail.com>
2022-05-11 22:55:08 +0900

    Update piano-stair-music.py

--- piano-stair-music.py
+++ piano-stair-music.py
@@ -13,8 +13,6 @@ socket = BluetoothSocket(RFCOMM)
 socket.connect(("블루투스 주소", 1))
 print("bluetooth connected!")
 
-data = ""
-
 pygame.mixer.init(frequency=44100, size=-16, channels=1, buffer=2 ** 12)
 
 channel1 = pygame.mixer.Channel(0)
@@ -42,63 +40,55 @@ def do_play():
         if list[1] == '1':
             channel1.play(do)
             time.sleep(0.08)
-            data = ""
 
 
 def re_play():
     while True:
-        if list[2] == '1':
+        if lst[2] == '1':
             channel2.play(re)
             time.sleep(0.08)
-            data = ""
 
 
 def mi_play():
     while True:
-        if list[3] == '1':
+        if lst[3] == '1':
             channel3.play(mi)
             time.sleep(0.08)
-            data = ""
 
 
 def fa_play():
     while True:
-        if list[4] == '1':
+        if lst[4] == '1':
             channel4.play(fa)
             time.sleep(0.08)
-            data = ""
 
 
 def sol_play():
     while True:
-        if list[5] == '1':
+        if lst[5] == '1':
             channel5.play(sol)
             time.sleep(0.08)
-            data = ""
 
 
 def la_play():
     while True:
-        if list[6] == '1':
+        if lst[6] == '1':
             channel6.play(la)
             time.sleep(0.08)
-            data = ""
 
 
 def si_play():
     while True:
-        if list[7] == '1':
+        if lst[7] == '1':
             channel7.play(si)
             time.sleep(0.08)
-            data = ""
 
 
 def high_do_play():
     while True:
-        if list[8] == '1':
+        if lst[8] == '1':
             channel8.play(high_do)
             time.sleep(0.08)
-            data = ""
 
 
 threads = []
@@ -121,14 +111,13 @@ t8 = threading.Thread(target=high_do_play)
 t8.start()
 
 while True:
+    lst = []
     data = socket.recv(1024) # 1024 바이트만큼 데이터 읽어오기
     data = data.decode('utf-8') #utf-8 형식의 바이트 코드를 문자열로 변환
-    data = data.split(',') # 문자열을 ,로 쪼개기
-    lst = []
     lst = data
     print(lst)
-    time.sleep(0.08)
     #print("Received: %s" % data)
+    time.sleep(0.08)
     day = datetime.datetime.today().weekday()
 
     if day == 0 and change_st == 1:
